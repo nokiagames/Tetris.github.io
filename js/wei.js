11e60bcfe6097e78759adadfd7d4c5b5bdfd7a0b
nokiagames <nokia_games@yeah.net>
2024-12-08 20:40:07 +0800

    Delete js/wei.js

--- js/wei.js
+++
@@ -1,4 +0,0 @@
- if (/aliApp|dingtalk/gi.test(navigator.appVersion)) {
- window.document.body.innerHTML = ''
- window.document.write(`<div class="weui_msg"><div class="weui_icon_area"><i class=weui_icon_info weui_icon_msg"></i></div ><div class="weui_text_area"><h4 class="weui_msg_title"><br></h4></div><br><br><div class="copy-inner"><a href=""><span class="icopy"></span></a></div>`)
-}
